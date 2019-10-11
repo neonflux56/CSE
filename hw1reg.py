52008ab2481f729f1bd884c2a68714fba9df4e82
neonflux56 <ashishgupta63966@gmail.com>
2019-10-11 15:30:48 -0700

    V2

--- hw1reg.py
+++ hw1reg.py
@@ -19,5 +19,34 @@ for line in c:
         d['total_votes'] = int(d['total_votes'])
         dataset.append(d)
 
+c1 = 0
+c2 = 0
+c3 =0
+c4 = 0
+c5= 0
+ratings = [1,2,3,4,5]
+for d in dataset:
+    if d['star_rating']  == 1:
+        c1 += 1
+    if d['star_rating']  == 2:
+        c2 += 1
+    if d['star_rating']  == 3:
+        c3 += 1
+    if d['star_rating']  == 4:
+        c4 += 1
+    if d['star_rating']  == 5:
+        c5 += 1
 
-len(dataset)
+counts = [c1,c2,c3,c4,c5]
+counts
+
+ratingsdist = dict(zip(ratings,counts))
+
+X = list(ratingsdist.keys())
+Y = [ratingsdist[x] for x in X]
+
+print("Rating Distribution in dataset for " + str(ratings) +" is as follows:" + str(counts))
+plt.xlabel("Count")
+plt.ylabel("Rating")
+plt.title("Rating Distribution in dataset")
+plt.bar(X, Y)
