98edccb628c495a4fab08c4564d11335650e1a8b
neonflux56 <ashishgupta63966@gmail.com>
2019-10-11 14:57:17 -0700

    V1

--- hw1reg.py
+++ hw1reg.py
@@ -1,95 +1,23 @@
 
+import gzip
+import csv
+import matplotlib.pyplot as plt
 
-import numpy
-import urllib
-import scipy.optimize
-import random
-import ast
-
-def parseDataFromURL(fname):
-  for l in urllib.urlopen(fname):
-    yield eval(l)
-
-def parseData(fname):
-  for l in open(fname):
-    yield ast.literal_eval(l)
-
-print("Reading data...")
-# Download from http://jmcauley.ucsd.edu/cse255/data/beer/beer_50000.json"
-data = list(parseDataFromURL(" http://jmcauley.ucsd.edu/cse255/data/beer/beer_50000.json"))
-print("done")
-
-type(data)
-data[0]
-data_ = [d for d in data if 'user/ageInSeconds' in d and d['user/ageInSeconds']<80*365*24*60*60]
-len(data_)
+c = csv.reader(gzip.open("amazon_reviews_us_Gift_Card_v1_00.tsv.gz", 'r'), delimiter = '\t')
+dataset = []
+first = True
+for line in c:
+    # The first line is the header
+    if first:
+        header = line
+        first = False
+    else:
+        d = dict(zip(header, line))
+        # Convert strings to integers for some fields:
+        d['star_rating'] = int(d['star_rating'])
+        d['helpful_votes'] = int(d['helpful_votes'])
+        d['total_votes'] = int(d['total_votes'])
+        dataset.append(d)
 
 
-def feature(datum):
-    return [1,datum['user/ageInSeconds']]
-
-
-X = [feature(d) for d in data_]
-y = [d['review/overall'] for d in data_]
-
-theta = numpy.linalg.lstsq(X, y)
-
-
-
-### Convince ourselves that basic linear algebra operations yield the same answer ###
-
-X = numpy.matrix(X)
-y = numpy.matrix(y)
-numpy.linalg.inv(X.T * X) * X.T * y.T
-
-
-
-### How much do women prefer beer over men? ###
-
-data2 = [d for d in data if 'user/gender' in d]
-
-def feature(datum):
-  feat = [1]
-  if datum['user/gender'] == "Male":
-    feat.append(1)
-  else:
-    feat.append(0)
-  return feat
-
-X = [feature(d) for d in data2]
-y = [d['review/overall'] for d in data2]
-theta,residuals,rank,s = numpy.linalg.lstsq(X, y)
-theta
-### Gradient descent ###
-
-# Objective
-def f(theta, X, y, lam):
-  theta = numpy.matrix(theta).T
-  X = numpy.matrix(X)
-  y = numpy.matrix(y).T
-  diff = X*theta - y
-  diffSq = diff.T*diff
-  diffSqReg = diffSq / len(X) + lam*(theta.T*theta)
-  print("offset =", diffSqReg.flatten().tolist())
-  return diffSqReg.flatten().tolist()[0]
-
-# Derivative
-def fprime(theta, X, y, lam):
-  theta = numpy.matrix(theta).T
-  X = numpy.matrix(X)
-  y = numpy.matrix(y).T
-  diff = X*theta - y
-  res = 2*X.T*diff / len(X) + 2*lam*theta
-  print("gradient =", numpy.array(res.flatten().tolist()[0]))
-  return numpy.array(res.flatten().tolist()[0])
-
-scipy.optimize.fmin_l_bfgs_b(f, [0,0], fprime, args = (X, y, 0.1))
-
-### Random features ###
-
-def feature(datum):
-  return [random.random() for x in range(30)]
-
-X = [feature(d) for d in data2]
-y = [d['review/overall'] for d in data2]
-theta,residuals,rank,s = numpy.linalg.lstsq(X, y)
+len(dataset)
